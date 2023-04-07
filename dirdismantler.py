1dc0fe707cd87b483a9f69fdac93e3cc4804e12e
[Silverainox] <serainox@gmail.com>
2023-04-07 03:41:58 +0200

    Update dirdismantler.py

--- dirdismantler.py
+++ dirdismantler.py
@@ -8,7 +8,7 @@ def load_config(config_file):
     return config
 
 def move_files(source_path, target_path, log_file):
-    log('Moving files...', log_file)
+    log_messages = []
     for root, dirs, files in os.walk(source_path):
         for file in files:
             file_path = os.path.join(root, file)
@@ -26,33 +26,28 @@ def move_files(source_path, target_path, log_file):
             if not os.path.exists(target_path):
                 os.makedirs(target_path)
             shutil.move(file_path, target_file_path)
-            log(f"Moved file: {file_path} -> {target_file_path}", log_file)
+            log_messages.append(f"Moved file: {file_path} -> {target_file_path}")
+
+    return log_messages
 
 def remove_empty_folders(source_path, log_file):
-    log('Removing empty folders...', log_file)
+    log_messages = []
     for root, dirs, files in os.walk(source_path, topdown=False):
         for dir in dirs:
             dir_path = os.path.join(root, dir)
             if not os.listdir(dir_path):
                 os.rmdir(dir_path)
-                log(f"Removed empty folder: {dir_path}", log_file)
+                log_messages.append(f"Removed empty folder: {dir_path}")
 
-def log(message, log_file):
+    return log_messages
+
+def log(log_messages, log_file):
     log_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
     if not os.path.exists(log_folder):
         os.makedirs(log_folder)
     log_file_path = os.path.join(log_folder, log_file)
-    with open(log_file_path, 'a') as f:
-        f.write(message + '\n')
-    print(message)
-
-if __name__ == '__main__':
-    config_file = 'config.yml'
-    config = load_config(config_file)
-    source_path = config['source_path']
-    target_path = config['target_path']
-    log_file = 'log.txt'
-    if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs', log_file)):
+    if os.path.exists(log_file_path):
+        # If log file already exists, append a numerical suffix
         i = 1
         while True:
             new_file_name = f"log-{i}.txt"
@@ -61,17 +56,27 @@ if __name__ == '__main__':
                 log_file = new_file_name
                 break
             i += 1
+    with open(os.path.join(log_folder, log_file), 'w') as f:
+        f.write('\n'.join(log_messages) + '\n')
+    print('Log file generated successfully:', os.path.join(log_folder, log_file))
+
+if __name__ == '__main__':
+    config_file = 'config.yml'
+    config = load_config(config_file)
+    source_path = config['source_path']
+    target_path = config['target_path']
 
     if not os.path.exists(source_path):
-        log(f"Source path '{source_path}' does not exist. Exiting...", log_file)
+        print(f"Source path '{source_path}' does not exist. Exiting...")
         exit(1)
 
     if not os.path.exists(target_path):
-        log(f"Target path '{target_path}' does not exist. Creating directory...", log_file)
+        print(f"Target path '{target_path}' does not exist. Creating directory...")
         os.makedirs(target_path)
 
-    move_files(source_path, target_path, log_file)
+    log_messages = move_files(source_path, target_path, 'log.txt')
+    log_messages += remove_empty_folders(source_path, 'log.txt')
 
-    remove_empty_folders(source_path, log_file)
-
-    print('Files moved successfully!')
+    log(log_messages, 'log.txt')
+os.system('cls' if os.name == 'nt' else 'clear')
+print('\n',' Files moved successfully!','\n',' Folder directories are no more','\n','\n',' SZMELC COMMANDER','\n')
